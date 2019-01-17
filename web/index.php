cf7120e0288410c36a21291ec82d1db389ffba58
Mahdi BOUZEBOUDJA <mahdibouzeboudja@yahoo.fr>
2019-01-17 17:37:23 +0100

    Connection to mysql container

--- web/index.php
+++ web/index.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "172.22.0.2";
+$servername = "172.25.0.2";
 $username = "root";
 $password = "test";
-$dbname = "myDB";
+$dbname = "Docker";
 
 //Create connection
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
@@ -15,34 +15,4 @@ catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
-
-// // Create database
-// $sql = "CREATE DATABASE myDB";
-// if ($conn->query($sql) === TRUE) {
-//     echo "Database created successfully";
-// } else {
-//     echo "Error creating database: " . $conn->error;
-// }
-// $conn->close();
-
-
-// Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-//
-// $sql = "SELECT id, firstname, lastname FROM MyGuests";
-// $result = $conn->query($sql);
-//
-// if ($result->num_rows > 0) {
-//     // output data of each row
-//     while($row = $result->fetch_assoc()) {
-//         echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-//     }
-// } else {
-//     echo "0 results";
-// }
-// $conn->close();
 ?>
