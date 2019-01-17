1b78ea13b368f412c1395be9bb67a20e103a1301
Mahdi BOUZEBOUDJA <mahdibouzeboudja@yahoo.fr>
2019-01-17 23:58:49 +0100

    last commit

--- web/index.php
+++ web/index.php
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully to databse container";
 
 // Create database.
 if(!$conn->select_db($dbname)){
@@ -40,9 +40,14 @@ reg_date TIMESTAMP
 )";
 
 if ($conn->query($sql) === TRUE) {
+    echo "<div>";
     echo "Table User created successfully";
+    echo "</div>";
+
 } else {
+    echo "<div>";
     echo "Error creating table: " . $conn->error;
+    echo "</div>";
 }
 
 //Insert data
@@ -50,7 +55,9 @@ $sql = "INSERT INTO User (firstname, lastname, email)
 VALUES ('Dupond', 'Doe', 'Dupond@example.com')";
 
 if ($conn->query($sql) === TRUE) {
+    echo "<div>";
     echo "New record created successfully";
+    echo "</div>";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
@@ -63,15 +70,17 @@ echo "<tr>";
     echo "<th>email</th>";
 echo "</tr>";
    while($row = $ret->fetch_array(MYSQLI_ASSOC) ) {
+      echo "<tr>";
       echo "<td> ". $row['firstname'] ."</td>" ;
       echo "<td>". $row['lastname'] ."</td>";
       echo "<td>".$row['email'] ."</td>";
+      echo "</tr>";
+
    }
-     echo "</table>";
-   echo "Operation done successfully\n";
-
-
-
+echo "</table>";
+echo"<div>";
+echo "Operation done successfully\n";
+echo "</div>";
 $conn->close();
 
 
