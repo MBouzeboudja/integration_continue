94b86b583cd54bf8b2365c817b3213d0803ce9de
Mahdi BOUZEBOUDJA <mahdibouzeboudja@yahoo.fr>
2019-01-17 23:47:12 +0100

    Recover data from db container

--- web/index.php
+++ web/index.php
@@ -1,18 +1,79 @@
 <?php
-$servername = "172.25.0.2";
+$servername = "mysql";
 $username = "root";
 $password = "test";
-$dbname = "Docker";
+$dbname = "test1";
 
-//Create connection
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+// Create connection.
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection.
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Create database.
+if(!$conn->select_db($dbname)){
+  echo "data base not exist";
+
+  $sql = "CREATE DATABASE " . $dbname;
+  if ($conn->query($sql) === TRUE) {
+      echo "Database created successfully";
+  } else {
+      echo "Error creating database: " . $conn->error;
+  }
+
+  //Select Database after creation.
+  if(!$conn->select_db($dbname)){
+    echo "failled select databse";
+  }
+}
+
+//Creation of table
+$sql = "CREATE TABLE User (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table User created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+//Insert data
+$sql = "INSERT INTO User (firstname, lastname, email)
+VALUES ('Dupond', 'Doe', 'Dupond@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$sql = "SELECT * FROM User";
+$ret = $conn->query($sql);
+echo "<table>";
+echo "<tr>";
+    echo "<th>first_name</th>";
+    echo "<th>last_name</th>";
+    echo "<th>email</th>";
+echo "</tr>";
+   while($row = $ret->fetch_array(MYSQLI_ASSOC) ) {
+      echo "<td> ". $row['firstname'] ."</td>" ;
+      echo "<td>". $row['lastname'] ."</td>";
+      echo "<td>".$row['email'] ."</td>";
+   }
+     echo "</table>";
+   echo "Operation done successfully\n";
+
+
+
+$conn->close();
+
+
+
 ?>
